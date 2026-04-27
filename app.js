bf40167e6d931d1995bd9f8e361c2880d1d82759
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-04-27 10:39:17 +0330

    add manage PATCH

--- app.js
+++ app.js
@@ -50,6 +50,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+//manage PATCH
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 //manage POST
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);

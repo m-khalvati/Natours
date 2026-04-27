6e14fddc68ce103545677e921c39a10c4319dc48
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-04-27 13:07:40 +0330

    Create Our MilddleWare & add Morgan MW & Implement Users Routes

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware (hand)');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
 
+// 2) ROUTE HANDLERS
+//// TOURS REQUESTS
+
 const getAllTours = (req, res) => {
-  res.starus(200).json({
+  console.log(req.requestTime);
+  res.status(200).json({
     status: 'success',
+    requestAr: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -18,7 +37,6 @@ const getAllTours = (req, res) => {
   });
 };
 
-// manage GET
 const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
@@ -39,10 +57,7 @@ const getTour = (req, res) => {
   });
 };
 
-//manage POST
 const createTour = (req, res) => {
-  // console.log(req.body);
-
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -62,7 +77,6 @@ const createTour = (req, res) => {
   );
 };
 
-//manage PATCH
 const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -79,7 +93,6 @@ const updateTour = (req, res) => {
   });
 };
 
-//manage DEL
 const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
@@ -94,18 +107,62 @@ const deleteTour = (req, res) => {
   });
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+//// USER REQUESTS
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+// 3) ROUTES
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {

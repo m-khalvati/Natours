a1553e8887dc100660cb612f36a983c3558e714c
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-04-27 09:08:08 +0330

    add Manage POST

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -13,10 +15,11 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
-// manage GET
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
+
+// manage GET
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -25,6 +28,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+//manage POST
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 const port = 3000;

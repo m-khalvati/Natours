0045db285c39345102d94bfc54e884370ac52808
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-04-30 14:21:21 +0330

    add Mongoose & Create a simple Tour Model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
 exports.checkID = (req, res, next, val) => {
@@ -9,7 +9,7 @@ exports.checkID = (req, res, next, val) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid ID'
     });
   }
   next();
@@ -19,7 +19,7 @@ exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: 'Missing name or price'
     });
   }
   next();
@@ -32,28 +32,28 @@ exports.getAllTours = (req, res) => {
     requestAr: req.requestTime,
     results: tours.length,
     data: {
-      tours,
-    },
+      tours
+    }
   });
 };
 
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  const tour = tours.find(el => el.id === id);
 
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid ID'
     });
   }
 
   res.status(200).json({
     status: 'success',
     data: {
-      tour,
-    },
+      tour
+    }
   });
 };
 
@@ -66,14 +66,14 @@ exports.createTour = (req, res) => {
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    err => {
       res.status(201).json({
         status: 'success',
         data: {
-          tour: newTour,
-        },
+          tour: newTour
+        }
       });
-    },
+    }
   );
 };
 
@@ -81,14 +81,14 @@ exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>',
-    },
+      tour: '<Updated tour here...>'
+    }
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: null
   });
 };
--- server.js
+++ server.js
@@ -1,10 +1,58 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(process.env);
-// console.log(app.get('env'));
+// mongodb.com
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// local mongo
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The b Park Camper',
+  price: 996
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR 💥:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

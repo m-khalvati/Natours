874ffec2ec000fe618314af72449b89e6da09bf4
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-05-02 19:28:50 +0330

    global Error Handling Middleware + refactoring Files

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -67,7 +67,7 @@ exports.createTour = async (req, res) => {
       }
     });
   } catch (err) {
-    res.status(404).json({
+    res.status(400).json({
       status: 'fail',
       message: err
     });

cc70fc34b9821e255c4c3bb1bd232d9ed0ddbaa0
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-05-03 16:17:27 +0330

    Handling Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,6 +13,13 @@ const handleDuplicateFieldsDB = err => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('.')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.message,
@@ -54,6 +61,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
 
     sendErrorProd(err, res);
   }

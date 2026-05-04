c27875c04a1e740be0509adfe15530e4d1993dbb
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-05-04 15:38:48 +0330

    Protecting Tour Routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -20,9 +20,15 @@ const handleValidationErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
-    status: err.message,
+    status: err.status,
     error: err,
     message: err.message,
     stack: err.stack
@@ -59,10 +65,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    if (err.name === 'CastError') err = handleCastErrorDB(err);
-    if (err.code === 11000) err = handleDuplicateFieldsDB(err);
-    if (err.name === 'ValidationError') err = handleValidationErrorDB(err);
+    let error = { ...err };
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    if (err.name === 'JsonWebTokenError') error = handleJWTError();
+    if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-    sendErrorProd(err, res);
+    sendErrorProd(error, res);
   }
 };

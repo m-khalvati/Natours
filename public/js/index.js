e216b3fd4ed766a49cf69767b11870b1097385d8
m-khalvati <139928036+m-khalvati@users.noreply.github.com>
2026-05-17 01:28:29 +0330

    add Sendgrid Emails, add Stripe Payments Back/Front, Model Bookings, Create New Booking on Checkout Success

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,6 +11,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -37,7 +39,7 @@ if (userDataForm)
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
     console.log(form);
-    
+
     updateSettings(form, 'data');
   });
 
@@ -58,4 +60,10 @@ if (userPasswordForm)
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
+  });
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
   });
